68d18c6b227d9c6142b656ef2dc1890e4b69f07e
Nathan Howard <nhoward.iit@gmail.com>
2026-02-23 10:54:53 -0800

    good enough

--- script.js
+++ script.js
@@ -1,18 +1,50 @@
+const defaultValue = 10;
+const colorShift = 5;
+const lighnessShift = -5;
+
+let hue = 0;
+
 const game = document.querySelector(".game");
 const inputElement = document.querySelector("input");
 const resetButton = document.querySelector("button");
-const defaultValue = 250;
 
 inputElement.value = defaultValue;
 makeGrids(defaultValue);
 
 resetButton.addEventListener("click", resetGame);
 
+// clears the input text box when user clicks
+inputElement.addEventListener("click", (event) => {
+    event.preventDefault();
+    inputElement.value = "";
+});
+
+function colorGrid(event) {
+    let bgColor = event.target.style.backgroundColor;
+    if (!bgColor) {
+        event.target.style.backgroundColor = `hsl(${hue} 80% 60% )`;
+        hue += colorShift;
+    } else {
+        // let newColor = darkenHSLColor(bgColor);
+        let newColor = `hsl( from ${bgColor} h s calc( l - 10%) )`;
+        console.log(newColor);
+        event.target.style.backgroundColor = newColor;
+    }
+}
+
+function darkenHSLColor(hslStr) {
+    const [hue, saturation, lightness] = hslStr.match(/\d+/g).map(Number);
+
+    const newLightness = Math.max(0, Math.min(100, lightness + lighnessShift));
+
+    return `hsl(${hue}, ${saturation}%, ${newLightness}%)`;
+}
+
 function resetGame(event) {
     event.preventDefault();
     let inputValue = inputElement.value;
-    if ((!inputValue) instanceof Number) {
-        inputElement.value = "Enter Valid Number";
+    if (inputValue > 250) {
+        inputElement.value = "Enter a number < 250";
         return;
     }
     makeGrids(inputValue);
@@ -27,8 +59,6 @@ function makeGrids(numberOfGridSqures) {
         grids.style.flexBasis = `${(1 / numberOfGridSqures) * 100}%`;
         game.appendChild(grids);
 
-        grids.addEventListener("mouseenter", (event) => {
-            event.target.style.backgroundColor = "black";
-        });
+        grids.addEventListener("mouseenter", colorGrid);
     }
 }
